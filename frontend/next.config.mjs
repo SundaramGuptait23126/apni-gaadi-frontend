e6dd7955fd122be65514974b11ab1009cfd5088c
Sundaram Gupta <sundramgupta990@gmail.com>
2026-08-08 00:54:04 +0530

    Update API URLs to relative paths and fix Vercel proxy destination

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://54.79.164.28/api/:path*',
+        destination: 'http://16.176.228.203/api/:path*',
       },
     ];
   },
